15cf9bc40cb3bf2c30e1ccdd2f0e608a2113765b
kodir-zaelani <kodir.zaelani78@gmail.com>
2026-04-07 00:30:18 +0800

    detail halaman

--- resources/views/themes/aksataedu/static/page-detail.blade.php
+++ resources/views/themes/aksataedu/static/page-detail.blade.php
@@ -20,16 +20,22 @@
         <div class="row">
             <div class="col-xl-9 col-md-7 col-12">
                 <div class="box">
-                    <div class="box-body">
+                    <div class="box-body blog-post">
                         <h3 class="box-title">{{ $item->title }}</h3>
-                        <div class="cour-stac d-lg-flex align-items-center text-fade">
+                        <div class="mb-10 entry-meta">
+                            <ul class="list-unstyled">
+                                <li><a href="#"><i class="fa fa-eye me-5"></i> {{$item->view_count}} kali</a></li>
+                                <li><a href="#"><i class="fa fa-book-open me-5"></i> {{ $item->reading_time }} menit</a></li>
+                            </ul>
+                        </div>
+                        {{-- <div class="cour-stac d-lg-flex align-items-center text-fade">
                             <div class="d-flex align-items-center">
                                 <p><i class="fa fa-book-open text-muted me-5"></i> {{ $item->reading_time }} menit</p>
                                 <p class="lt-sp">|</p>
                                 <p><i class="fa fa-calendar-o me-5"></i> {{$item->published_at ? $item->published_at : $item->created_at}}</p>
 
                             </div>
-                        </div>
+                        </div> --}}
                         <img class="py-10 box-img-top btrr-5 btlr-5" src="{{$item->image_url ? $item->image_url : asset('uploads/images/logo/'. $global_option->logo)}}" alt="Card image cap">
                         <hr>
                         <p class="fs-16 mb-30">
