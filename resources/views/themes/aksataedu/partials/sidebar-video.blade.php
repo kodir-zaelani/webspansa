c24f5000118d13289e37b368181a34597424ae8f
kodir-zaelani <kodir.zaelani78@gmail.com>
2026-04-07 09:20:18 +0800

    sidebar video

--- resources/views/themes/aksataedu/partials/sidebar-video.blade.php
+++ resources/views/themes/aksataedu/partials/sidebar-video.blade.php
@@ -9,14 +9,16 @@
             <div class="row">
                 @forelse ($global_videos as $item)
                 <div class="px-1 py-1 col-md-12 col-sm-12">
-                    <a href="#" title="{{$item->title}}" class="pb-0 text-center box bg-img py-50 pull-up" href="javascript:void(0)" style="background-image: url({{ $item->imageUrl ? $item->imageUrl : '/uploads/images/logo/' . $global_option->logo }})">
-						<div class="pb-0 box-body">
-							<span class="badge badge-primary">Video</span>
-						</div>
-						<div class="box-body py-15 bg-black-70 rounded-0">
-							<h4 class="text-white">{{$item->title}}</h4>
-						</div>
-					</a>
+                    <div class="box bg-img pull-up" style="background-image: url({{ $item->imageUrl ? $item->imageUrl : '/uploads/images/logo/' . $global_option->logo }})">
+                        <div class=" box-body">
+                            <a href="{{$item->video}}" title="{{$item->title}}" class="text-center btn btn-circle btn-primary popup-youtube play-vdo-bt"  >
+                                <i class="mdi mdi-play"></i>
+                            </a>
+                        </div>
+                        <div class="py-10 box-body mt-80 bg-black-70 rounded-0">
+                            <h4 class="text-white">{{ Str::limit($item->title, 75) }}</h4>
+                        </div>
+                    </div>
                 </div>
                 @empty
                 <p>No videos available.</p>
--- resources/views/themes/aksataedu/post/main-news-new.blade.php
+++ resources/views/themes/aksataedu/post/main-news-new.blade.php
@@ -11,7 +11,7 @@
                     <div class="box-body">
                         <div class="row justify-content-center">
                             @foreach ($featured_news as $item)
-                            <div class="mb-4 col-xl-6 col-lg-6 col-md-6 col-sm-12 d-none d-md-block d-lg-block d-xl-block">
+                            <div class="mb-4 col-xl-6 col-lg-6 col-md-6 col-sm-12 ">
                                 <div class="mb-2 border-0 card">
                                     <div class="row g-0">
                                         <div class="col-md-4 ">
