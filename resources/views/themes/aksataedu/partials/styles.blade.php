394623e7dd76e1e1085426ae3653b57023f24923
kodir-zaelani <kodir.zaelani78@gmail.com>
2026-04-08 12:40:37 +0800

    up meta front end

--- resources/views/themes/aksataedu/partials/styles.blade.php
+++ resources/views/themes/aksataedu/partials/styles.blade.php
@@ -1,12 +1,43 @@
+
 @if ($global_option->favicon)
 <link rel="icon" href="{{ asset('') }}uploads/images/logo/{{ $global_option->favicon }}" rel="icon">
 @else
 <link rel="icon" href="{{asset('')}}assets/images/favicon.ico">
 @endif
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
+@if (Request::segment(1) == '')
+<meta property="og:type" content="article"/>
+@if ($global_option->logo != null)
+<meta property="og:image" content="{{ $global_option->imageThumbUrl }}" />
+@endif
+@elseif (Request::segment(1) != null)
+<meta property="og:type" content="article"/>
+@if ($item->image != null)
+<meta property="og:image" content="{{ $item->imageThumbUrl }}" />
+@endif
+@endif
+
+@if ($global_option->webname != null)
+<meta property="og:title" content="{{ $global_option->webname }}"/>
+@else
+<meta name="og:title" content="Maroko Kreatif">
+@endif
+@if ($global_option->meta_keywords != null)
+<meta name="keywords" content="{{ $global_option->meta_keywords }}">
+@else
+<meta name="keywords"
+content="Digital Nusantara, Digital Nusantara Borneo, Borneo, Digital, Nusantara, Kaltim">
+@endif
+@if ($global_option->meta_description != null)
+<meta name="description" content="{{ $global_option->meta_description }}">
+@else
+<meta name="description"
+content="Digital Nusantara, Digital Nusantara Borneo, Borneo, Digital, Nusantara, Kaltim">
+@endif
+
 <meta name="author" content="">
 {{ isset($slot) ? $slot : null }}
 <title>{{ $title ?? config('app.name', 'Anak Petani') }}</title>
